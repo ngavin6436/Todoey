23bd2d33ae0fa15b9ce472aa79e03c4a9debf6d3
Nicholas Gavin <surfwithnick@yahoo.com>
2018-12-14 00:34:37 -0500

    Persist new items added

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,9 +12,15 @@ class ToDoListViewController: UITableViewController {
 
     
     var myArray = ["The first Item", "Second one", "Another one"]
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            myArray = items
+        }
     }
 
     
@@ -51,8 +57,10 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what happens when user clicks "add"
-            print("Success")
+
             self.myArray.append(textEntered.text ?? "")
+            self.defaults.set(self.myArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
 
         }
